dc0b1bf3b2f7267b8ab66ae1324f21cae3837d14
axdill7 <mohammadadilayub7@gmail.com>
2022-09-19 19:16:24 +0530

    Mypython learning project

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+from datetime import date, datetime
+from sqlite3 import Date
+
+
+# Python3 program to add two numbers
+
+num1 = 12.5
+num2 = 12
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+
+for i<10, i++
